a70ea01d2af93c0b7f2128212aae41e2d9547f51
Junzhe Liang <junzhe.liang@bellevuecollege.edu>
2019-01-22 15:49:33 -0800

    Update FindBus.java

--- FindBus.java
+++ FindBus.java
@@ -7,12 +7,16 @@ import java.io.*;
 import java.nio.*;
 import java.lang.*;
 import java.util.*;
-public class FindBus {
 
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
+
+public class FindBus {
+    public static Multimap<String,String> hmap = ArrayListMultimap.create();
     FindBus() throws Exception{
-        showRoutes();
+        getRoutes();
     }
-    void showRoutes() throws Exception{
+    void getRoutes() throws Exception{
         URLConnection web = new URL("https://www.communitytransit.org/busservice/schedules/").openConnection();
         web.setRequestProperty("user-Agent","Mozilla/5.0(Windows  NT 6.1; WOW64) AppleWebKit/537.11(KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
         BufferedReader in = new BufferedReader(new InputStreamReader(web.getInputStream(), Charset.forName("UTF-8")));
@@ -28,31 +32,33 @@ public class FindBus {
         Matcher m = p.matcher(text);
         m.find();
         String text2 = m.group();
-        Pattern p2 = Pattern.compile("<option value=\"(\\d+[-]?\\d*)\">(\\d)*\\s-\\s(.*)?(amp;)(.*)to\\s(.*)</option>");
-        m = p2.matcher(text2);
+        //Pattern p2 = Pattern.compile("<option value=\"(\\d+)\">(\\d)*\\s-\\s(.*)\\sto(\\s)(.*)</option>");
+        Pattern p2 = Pattern.compile("<option value=\"(\\d*|\\c*)-?(\\d*|\\c*)?\">(\\d*|\\c*]*)/?(\\d*|\\c*)?\\s-\\s(.*)\\s?(to|-)\\s(.*)</option>");
         int n = -2;
-        HashMap<Integer,String> hmap = new HashMap<Integer,String>();
+        text2 = text2.replaceAll("&amp;","&");
+        m = p2.matcher(text2);
         while(m.find()){
             n++;
+            if(!m.group(2).equals("")){
+                hmap.put(m.group(7),m.group(1));
+                hmap.put(m.group(7),m.group(2));
+            }else{
+                hmap.put(m.group(7),m.group(1));
 
-            //System.out.println(m.group(1));
-            //System.out.println(m.group(3) + m.group(5));
+            }
+            //g1:bus num    g3:start  g4:end
+           // System.out.println(m.group(5));
+           // System.out.println(m.group(1));
+           // System.out.println(m.group(2));
+           // System.out.println("-----------------------");
+
             //System.out.println(m.group(6));
-            //g1:bus num    g3g5:start  g6:end
-            int a = Integer.valueOf(m.group(1));
-            System.out.println(a);
-            hmap.put(a,m.group(6));
-
         }
-        Set set = hmap.entrySet();
-        Iterator iterator = set.iterator();
-        while(iterator.hasNext()) {
-            Map.Entry mentry = (Map.Entry)iterator.next();
-            System.out.print("key is: "+ mentry.getKey() + " & Value is: ");
-            System.out.println(mentry.getValue());
-        }
-        //System.out.println(n);
-        //System.out.println(text2);
-
     }
+
 }
+
+
+
+//System.out.println(n);
+//System.out.println(text2);
