7fca1e681510c1d7189c47344347b485d5f22ee7
Junzhe Liang <junzhe.liang@bellevuecollege.edu>
2019-01-23 12:07:15 -0800

    Update FindSchedule.java

--- FindSchedule.java
+++ FindSchedule.java
@@ -1,22 +1,24 @@
 
 import java.nio.charset.Charset;
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.regex.*;
 import java.net.*;
 import java.io.*;
-import java.nio.*;
 import java.lang.*;
 import java.util.*;
 
 public class FindSchedule {
     String num;
     String text = "";
+
     FindSchedule(String n)throws Exception {
         num = n;
         loadPage();
         showSchedule();
     }
+    //----------------------------------------------------------------------------------------------------
+    /*
+    Download the html
+     */
     void loadPage()throws Exception{
         String theurl= "https://www.communitytransit.org/busservice/schedules/route/"+num;
         System.out.println(theurl);
@@ -27,41 +29,42 @@ public class FindSchedule {
             text += inputLine + "\n";
         }
         in.close();
-        //System.out.println(text);
     }
-
-    void showSchedule(){
+    //-----------------------------------------------------------------------------------------------------
+    /*
+    Use Reg the find the stops and directions, use '-' to differentiate
+     */
+    void showSchedule() {
         Pattern p = Pattern.compile("(<p>(.*)</p>)|(<h2>(.*)<small>(.*)</small></h2>)");
         Matcher m = p.matcher(text);
         List<String> sta = new ArrayList<>();
         String temp = "";
-        while(m.find()){
-            //System.out.println(m.group());
-        //System.out.println(m.group(1));
-            if(m.group().contains("<h2>")){
-                 temp = m.group(4) +"  "+ m.group(5);
-                 temp = "------------" + temp +"-----------";
-            }else{
-                 temp = m.group(2).replaceAll("&amp;","&");
+        while (m.find()) {
+            if (m.group().contains("<h2>")) {
+                temp = m.group(4) + "-- " + m.group(5);
+                temp = "------------" + temp + "-----------";
+            } else {
+                temp = m.group(2).replaceAll("&amp;", "&");
             }
-        sta.add(temp);
+            sta.add(temp);
         }
         int count = 1;
-        for(int i = 1; i<sta.size()-1;i++){
-            if(!sta.get(i).contains("-")){
-                System.out.println("Stop"+count+" "+sta.get(i));
+        /*
+        Print the result, since i used a indistinct reg above, i need some ifs to avoid printing useless things
+         */
+        for (int i = 1; i < sta.size() - 1; i++) {
+            if (sta.get(i).contains("<")) {
+                continue;
+            } else if (!sta.get(i).contains("---")) {
+                System.out.println("Stop" + count + " " + sta.get(i));
                 count++;
-            }else{
+            } else {
                 System.out.println(sta.get(i));
                 count = 1;
             }
-
-
+        }
+    }
 
         }
-       // m.find();
-       // String part2 = m.group();
-       // System.out.println(part1);
 
-    }
-}
+
