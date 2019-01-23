a2348738e39a642ad4db9a9bb4a8858a53786240
Junzhe Liang <junzhe.liang@bellevuecollege.edu>
2019-01-23 11:38:43 -0800

    Update FindBus.java

--- FindBus.java
+++ FindBus.java
@@ -34,22 +34,24 @@ public class FindBus {
         m.find();
         String text2 = m.group();
         //Pattern p2 = Pattern.compile("<option value=\"(\\d+)\">(\\d)*\\s-\\s(.*)\\sto(\\s)(.*)</option>");
-        Pattern p2 = Pattern.compile("<option value=\"(\\d*|\\c*)-?(\\d*|\\c*)?\">(\\d*|\\c*]*)/?(\\d*|\\c*)?\\s-\\s(.*)\\s?(to|-)\\s(.*)</option>");
+        Pattern p2 = Pattern.compile("<option value=\"((\\d*|\\c*)-?(\\d*|\\c*)?)\">((\\d*|\\c*)-?(\\d*|\\c*)?)/?(\\d*|\\c*)?\\s-\\s(.*)\\s?(to|-)\\s(.*)</option>");
         int n = -2;
         text2 = text2.replaceAll("&amp;","&");
         m = p2.matcher(text2);
         while(m.find()){
             n++;
-            if(!m.group(2).equals("")){
+            /*if(!m.group(2).equals("")){
                 hmap.put(m.group(7),m.group(1));//7 des
                 hmap.put(m.group(7),m.group(2));
-            }else{
-                hmap.put(m.group(7),m.group(1));
-            }
+            }else{*/
+            System.out.println(m.group(10));
+            System.out.println(m.group(1));
+                hmap.put(m.group(10),m.group(1));
+            //}
         }
-        //System.out.println(hmap.keySet().toString());
+        System.out.println(hmap.keySet().toString());
     }
-    void chooseRoute(){
+    void chooseRoute()throws Exception{
         Scanner scan = new Scanner(System.in);
         System.out.println("Please type your destinations");
         String input;
@@ -64,7 +66,9 @@ public class FindBus {
         System.out.println(m3.group(1));
         System.out.println(hmap.get(m3.group(1)));
         }
-
+        System.out.println("Bus Number:");
+        input = scan.next();
+    new FindSchedule(input);
     }
 
 }
