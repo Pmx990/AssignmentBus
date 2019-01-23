16ca39fbdfab0e34df0e4f2685df3d2fb0dfe1bd
Junzhe Liang <junzhe.liang@bellevuecollege.edu>
2019-01-22 16:31:45 -0800

    Update FindBus.java

--- FindBus.java
+++ FindBus.java
@@ -15,6 +15,7 @@ public class FindBus {
     public static Multimap<String,String> hmap = ArrayListMultimap.create();
     FindBus() throws Exception{
         getRoutes();
+        chooseRoute();
     }
     void getRoutes() throws Exception{
         URLConnection web = new URL("https://www.communitytransit.org/busservice/schedules/").openConnection();
@@ -40,20 +41,30 @@ public class FindBus {
         while(m.find()){
             n++;
             if(!m.group(2).equals("")){
-                hmap.put(m.group(7),m.group(1));
+                hmap.put(m.group(7),m.group(1));//7 des
                 hmap.put(m.group(7),m.group(2));
             }else{
                 hmap.put(m.group(7),m.group(1));
-
             }
-            //g1:bus num    g3:start  g4:end
-           // System.out.println(m.group(5));
-           // System.out.println(m.group(1));
-           // System.out.println(m.group(2));
-           // System.out.println("-----------------------");
-
-            //System.out.println(m.group(6));
         }
+        //System.out.println(hmap.keySet().toString());
+    }
+    void chooseRoute(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please type your destinations");
+        String input;
+        input = scan.next();
+        //input = "sea";
+        input = "[\\[,]\\s*("+input+"[a-zA-Z]*(\\s*[a-zA-Z]*)*)[,]";
+        Pattern p3 = Pattern.compile(input,Pattern.CASE_INSENSITIVE);
+        Matcher m3 = p3.matcher(hmap.keySet().toString());
+        System.out.println(input);
+        System.out.println(hmap.keySet().toString());
+        while(m3.find()){
+        System.out.println(m3.group(1));
+        System.out.println(hmap.get(m3.group(1)));
+        }
+
     }
 
 }
