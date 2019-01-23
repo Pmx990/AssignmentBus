30cdddf4967b12c42554c3478559eeb1f1842dec
Junzhe Liang <junzhe.liang@bellevuecollege.edu>
2019-01-23 12:38:47 -0800

    Update AppUi.java

--- AppUi.java
+++ AppUi.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 /*
 This is a program design to find and print the bus info from communitytransit.org
  */
 public class AppUi {
     public static void main(String[] args) throws Exception{
+        Scanner scan = new Scanner(System.in);
+        while (true){
             new FindBus();
+            System.out.println("Do you want another search?(Y/N)");
+            String input = scan.next();
+            if(input.equalsIgnoreCase("n")){
+                break;
+            }
+        }
+
     }
 
 }
