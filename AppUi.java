25db225e77edc7706b5bbad86d1f3cde9de7f604
Junzhe Liang <junzhe.liang@bellevuecollege.edu>
2019-01-23 12:07:11 -0800

    Update AppUi.java

--- AppUi.java
+++ AppUi.java
@@ -1,10 +1,9 @@
+/*
+This is a program design to find and print the bus info from communitytransit.org
+ */
 public class AppUi {
     public static void main(String[] args) throws Exception{
-
-
             new FindBus();
-
-
     }
 
 }
